06ac7deb9fe2675cd82ed10f487cb90bb4adec5e
Vidharshana Sri S S <vidharshanasri.ss@gmail.com>
2026-07-25 14:06:59 +0530

    Add files via upload

---
+++ assessment_9/c_05.c
@@ -0,0 +1,20 @@
+# include <stdio.h>
+int main(){
+    int arr[5],mem;
+    for(int x=0;x<5;x++){
+        scanf("%d",&arr[x]);
+    }
+    for (int i=0;i<5;i++){
+        for(int j=i+1;j<5;j++){
+            if(arr[i]>arr[j]){
+                mem=arr[i];
+                arr[i]=arr[j];
+                arr[j]=mem;
+            }
+        }
+    }
+    for (int k=0;k<5;k++){
+        printf("%d ",arr[k]);
+    }
+    return 0;
+}
